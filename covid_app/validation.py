f9291d2c64e741785e57ab087504571e41aff1a0
dalgarno <32097481+dalgarno@users.noreply.github.com>
2021-08-19 11:03:28 +0100

    refactor: use structure rather than from dict

--- covid_app/validation.py
+++ covid_app/validation.py
@@ -1,6 +1,9 @@
 from typing import Any, Dict
 
 import attr
+import cattr
+
+converter = cattr.Converter()
 
 
 @attr.s(auto_attribs=True, frozen=True)
@@ -10,15 +13,6 @@ class InputParams:
     weight: int
     underlying_health_issues: bool
     area_code: str
-
-    def from_dict(d: Dict[str, Any]):
-        return InputParams(
-            age_group=d["age_group"],
-            height=d["height"],
-            weight=d["weight"],
-            underlying_health_issues=d["underlying_health_issues"],
-            area_code=d["area_code"],
-        )
 
 
 def validate_input_params(data: Any) -> InputParams:
@@ -34,4 +28,4 @@ def validate_input_params(data: Any) -> InputParams:
         formatted_fields = ", ".join(missing_fields)
         raise ValueError(f"Missing required values: {formatted_fields}.")
 
-    return InputParams.from_dict(data)
+    return cattr.structure(data, InputParams)
